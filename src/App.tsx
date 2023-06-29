18347597ebb4a103989c4562b74af1d4404dbb64
Dmitry Arkhipov <arhidiman@mail.ru>
2023-06-29 21:33:33 +0700

    without squares

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './App.css'
+import gop from './images/me.jpg'
+
+function App() {
+
+    console.log(gop)
+    return(
+
+        <>
+             <div>Hello nigga</div>
+            
+        </>
+       
+        
+    )
+}
+
+
+export default App
